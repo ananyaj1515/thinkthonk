52885d8a862f82458696dbf40a552383c8ec2716
Ashmita Haldar <67264881+ashmitahaldar@users.noreply.github.com>
2025-08-30 19:21:49 +0800

    Add thumbnails to profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -2,7 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import TopNavbar from "./TopNavbar";
 import BottomNavbar from "./BottomNavbar";
-import pfp from "../pics/pfp.JPG"
+import pfp from "../videos/profile.jpg"
+import thumb1 from '../videos/thumbnail1.png';
+import thumb2 from '../videos/thumbnail2.png';
+import thumb3 from '../videos/thumbnail3.png';
+import thumb4 from '../videos/thumbnail4.png';
 
 function ProfilePage() {
   const navigate = useNavigate();
@@ -14,9 +18,10 @@ function ProfilePage() {
         <div className="profile-page">
           <div className="profile-header">
             <img
-              
+              src={pfp}
               alt="profile"
               className="w-24 h-24 rounded-full"
+              style={{ width: '96px', height: '96px', borderRadius: '50%' }}
             />
 
             <h2>@ananabanana</h2>
@@ -44,13 +49,12 @@ function ProfilePage() {
 
           
           <div className="grid">
-            {[...Array(9)].map((_, index) => (
+            {[thumb1, thumb2, thumb3, thumb4].map((imgUrl, index) => (
               <img
                 key={index}
-                src={`https://via.placeholder.com/150?text=Video+${
-                  index + 1
-                }`}
+                src={imgUrl}
                 alt={`video-${index}`}
+                style={{ width: '90px', height: '160px', objectFit: 'cover', borderRadius: '12px', margin: '8px' }}
               />
             ))}
           </div>
